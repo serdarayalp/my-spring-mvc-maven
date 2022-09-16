03b96b1db2a8febec8b8a6905f85c7e767eec42f
Serdar Ayalp <serdarayalp@googlemail.com>
2022-09-16 17:18:50 +0200

    Fundament

---
+++ src/main/java/com/regiocom/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.regiocom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getHome() {
+        return "home";
+    }
+
+    /*
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getHome(ModelMap modelMap) {
+        modelMap.addAttribute("message", "Das ist ein Test...");
+        return "home";
+    }
+    */
+}
